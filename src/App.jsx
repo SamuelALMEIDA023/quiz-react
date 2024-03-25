489f0c35141ae927b5772dfbcf8a7978ef300584
SamuelALMEIDA023 <sa_muelmarcos@hotmail.com>
2024-03-25 14:59:49 -0300

    feat: add global styles and fonts

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './styles/global.css'
+
+function MeuNome() {
+  return (
+    <h2>Samuel Almeida</h2>
+  )
+}
+
+function App() {
+  return (
+    <>
+    <h1>Hello World</h1>
+    <p>Tudo certo</p>
+    <button>
+      clique
+    </button>
+    <form action="option">
+    </form>
+    <input />
+    <select></select>
+    <MeuNome />
+    </>
+  )
+}
+
+export default App
